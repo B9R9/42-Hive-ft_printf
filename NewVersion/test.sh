e9a7f162de3e347a5e14082de8d50bc49abeb5be
Baptiste Riffard <briffard@c3r2p7.hive.fi>
2022-04-15 10:33:25 +0300

    AUTO SAVE

--- NewVersion/test.sh
+++ NewVersion/test.sh
@@ -16,4 +16,11 @@ if [ $1 = 'valgrind' ];then
     valgrind  --leak-check=full --show-leak-kinds=all test
     exit 1
 fi
+if [ $1 = 'push' ];then
+    cp -r /Users/briffard/Desktop/NewVersion /Users/briffard/Desktop/B9R9-ft_printf
+    cd /Users/briffard/Desktop/B9R9-ft_printf
+    git add NewVersion
+    git commit -m "AUTO SAVE"
+    git push
+fi
 
